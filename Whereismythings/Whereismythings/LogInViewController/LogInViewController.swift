f15254820e85bd51008ee115eb56253b325b4294
송낙현 <>
2021-01-15 17:11:59 +0900

    로그인 뷰 수정

--- Whereismythings/Whereismythings/LogInViewController/LogInViewController.swift
+++ Whereismythings/Whereismythings/LogInViewController/LogInViewController.swift
@@ -8,9 +8,39 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    
+    var loginData : [LoginInformations] = [
+        LoginInformations(id: "0524snh", email: "0524snh@coders.com", password: "0524snh1"),
+        LoginInformations(id: "0821yjh", email: "0821yjh@coders.com", password: "0821yjh1"),
+        LoginInformations(id: "0807khs", email: "0807khs@coders.com", password: "0807khs1")
+    ]
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+        //로그인 데이터랑 텍스트필드 데이터가 트루이면 프린트핼
+        
+        if loginData[0].email == emailTextField.text && loginData[0].password == passwordTextField.text {
+            
+            present(completeLogInViewController(), animated: true)
+        //여기는 왜 프린트가 안될까요
+            print(loginData[0].email)
+            
+        }
+     
+    }
+    
+    
 }
