aeba82597bebd18762c36ef25377c44a915eb341
JYP in MBP 15 <me@ebadaq.com>
2021-01-16 16:57:21 +0900

    menthoring 1

--- Whereismythings/Whereismythings/Informations.swift
+++ Whereismythings/Whereismythings/Informations.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 송낙현 on 2021/01/15.
 //
-
+import UIKit
 import Foundation
 
 
@@ -21,6 +21,19 @@ class LoginInformations {
         self.email = email
         self.password = password
     }
+    
+    var lostItems: [ItemInfo] = [ItemInfo(name: "에어팟맥스")]
+    var foundItems: [ItemInfo] = []
 }
 
+/// 사용자가 등록한 아이템의 정보
+struct ItemInfo {
+    let name: String // 물건 이름
+    let tag: [String] = [] // 맥미니, 멕미니, macMini, MacMini, MAC MINI, 애플 에플 
+    let images: [UIImage] = []
+//    let type: TypeOfItem
+}
 
+//enum TypeOfItem {
+//    case lost, found, wish
+//}
