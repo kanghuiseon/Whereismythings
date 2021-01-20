2084911e99f5f2872eeef0d4eef8ebe9a28881ae
kanghuiseon <ekfsla0113@naver.com>
2021-01-20 20:02:48 +0900

    mapview code reset #10

---
+++ Whereismythings/Whereismythings/Map/MapAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  MapAnnotation.swift
+//  Whereismythings
+//
+//  Created by 강희선 on 2021/01/20.
+//
+
+import Foundation
+import MapKit
+
+class MapAnnotation{
+   
+}
---
+++ Whereismythings/Whereismythings/Map/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Whereismythings
+//
+//  Created by 강희선 on 2020/12/26.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+class MapViewController: UIViewController{
+    @IBOutlet var mainMap: MKMapView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    var stuffs: [MainMapModel] = []
+    var locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        stuffs.append(MainMapModel(id: 1, stuffName: "맥미니", stuffCharacteristic: "새제품", stuffLatitudePosition: "37.5630725", stuffLongitudePosition: "127.0366688",stuffKoreanPosition: "성동구청", stuffImage: nil))
+        stuffs.append(MainMapModel(id: 2, stuffName: "맥북프로2020", stuffCharacteristic: nil, stuffLatitudePosition: "37.5112348", stuffLongitudePosition: "127.0980274", stuffKoreanPosition: "롯데월드", stuffImage: nil))
+        stuffs.append(MainMapModel(id: 3, stuffName: "에어팟", stuffCharacteristic: nil, stuffLatitudePosition: "37.5121422", stuffLongitudePosition: "126.9954074", stuffKoreanPosition: "세빛섬", stuffImage: nil))
+    }
+}
+
