6d14d07554eca8d372457b449b390d263807c3d6
kanghuiseon <ekfsla0113@naver.com>
2021-01-21 22:49:20 +0900

    현재 위치를 위한 location manager추가, gps버튼에 대한 target-action 추가 #10

--- Whereismythings/Whereismythings/Map/MapViewController.swift
+++ Whereismythings/Whereismythings/Map/MapViewController.swift
@@ -12,18 +12,30 @@ class MapViewController: UIViewController{
     @IBOutlet var mainMap: MKMapView!
     @IBOutlet var segmentedControl: UISegmentedControl!
     var stuffs: [MainMapModel] = []
-    var locationManager = CLLocationManager()
+    // 현재 기기 위치 관련 변수
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mainMap.showsUserLocation = true
+        
         stuffs.append(MainMapModel(id: 1, stuffName: "맥미니", stuffCharacteristic: "새제품", stuffLatitudePosition: "37.5630725", stuffLongitudePosition: "127.0366688",stuffKoreanPosition: "성동구청", stuffImage: nil))
         stuffs.append(MainMapModel(id: 2, stuffName: "맥북프로2020", stuffCharacteristic: nil, stuffLatitudePosition: "37.5112348", stuffLongitudePosition: "127.0980274", stuffKoreanPosition: "롯데월드", stuffImage: nil))
         stuffs.append(MainMapModel(id: 3, stuffName: "에어팟", stuffCharacteristic: nil, stuffLatitudePosition: "37.5121422", stuffLongitudePosition: "126.9954074", stuffKoreanPosition: "세빛섬", stuffImage: nil))
+        
+        
         for stuff in stuffs{
             let coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(stuff.stuffLatitudePosition)!, longitude: CLLocationDegrees(stuff.stuffLongitudePosition)!)
             let annotation = MapAnnotation(coordinate)
             mainMap.addAnnotation(annotation)
         }
+        
+    }
+    // gps, 현재 위치 주변 확대
+    @IBAction func btnCurrentPosition(_ sender: UIButton) {
         
     }
     @objc func btnDetailView(){
@@ -37,16 +49,14 @@ class MapViewController: UIViewController{
 extension MapViewController: MKMapViewDelegate{
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         //만약 사용자지정이면 return
-        if annotation.isEqual(MKUserLocation.self){
-            exit(0)
-        }
+        guard !annotation.isKind(of: MKUserLocation.self) else { return nil }
         let mapAnnotationView = MapAnnotationView(annotation: annotation, reuseIdentifier: nil)
         let detail = mapAnnotationView.detailCalloutAccessoryView as! MapPinView
         detail.btnPin.addTarget(self, action: #selector(btnDetailView), for: .touchUpInside)
         return mapAnnotationView
     }
-//    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
-//        let detailVC = storyboard?.instantiateViewController(identifier: "DetailStuffViewController")
-//        present(detailVC!, animated: true, completion: nil)
-//    }
+}
+
+extension MapViewController: CLLocationManagerDelegate{
+    
 }
