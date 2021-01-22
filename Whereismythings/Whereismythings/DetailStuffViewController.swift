c1684265e60e5b00832813d71fc4bb4b98bc9ba2
kanghuiseon <ekfsla0113@naver.com>
2021-01-23 02:15:43 +0900

    annotation view 클릭 시, 이미지 띄우기, 만약 이미지가 없다면 기본 카메라 이미지 띄우기 #13

--- Whereismythings/Whereismythings/DetailStuffViewController.swift
+++ Whereismythings/Whereismythings/DetailStuffViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class DetailStuffViewController: UIViewController {
     @IBOutlet var lblPerson: UILabel!
-    @IBOutlet var imgStuff: UIImage!
+    @IBOutlet var imgStuff: UIImageView!
     @IBOutlet var lblStuffName: UILabel!
     @IBOutlet var lblStuffPosition: UILabel!
     @IBOutlet var lblGotTime: UILabel!
@@ -20,23 +20,30 @@ class DetailStuffViewController: UIViewController {
     var time: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.lblPerson.text = person
-        self.imgStuff = img
-        self.lblStuffName.text = name
-        self.lblStuffPosition.text = position
-        self.lblGotTime.text = time
+        setValues()
+        
     }
     
 
-   
+    func setValues(){
+        self.lblPerson.text = person
+        if let img = img{
+            self.imgStuff.image = img
+        }
+        self.lblStuffName.text = name
+        self.lblStuffPosition.text = position
+        if let time = time{
+            self.lblGotTime.text = time
+        }
+    }
 
 }
 //extension DetailStuffViewController:  StuffInfoDelegate{
 extension DetailStuffViewController{
     func stuffInformation(lblPerson: String, imgStuff: UIImage?, lblStuffName: String, lblStuffPosition: String, lblGotTime: String?) {
         self.person = lblPerson
-        if let img = self.img{
-            self.imgStuff = img
+        if let img = imgStuff{
+            self.img = img
         }
         self.name = lblStuffName
         self.position = lblStuffPosition
