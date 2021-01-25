94bdfaefa4caaa05b2e950616fdb2c3d60a63f93
Kangheeseon <ekfsla0113@naver.com>
2021-01-25 22:34:24 +0900

    Revert "Feature/map (#18)" (#19)
    This reverts commit 19623ef2b9c5ec9c01ad3eb2bc72f223fdf51bf1.

---
+++ Whereismythings/Whereismythings/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Whereismythings
+//
+//  Created by 강희선 on 2020/12/26.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("test")
+    }
+
+
+}
+
