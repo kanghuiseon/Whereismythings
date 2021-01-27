3826af00c117a7ec2b94a21ffae78d8273435d4f
JeonghoYang <>
2021-01-28 00:23:16 +0900

    텍스트 필드 항목 추가

---
+++ Whereismythings/Whereismythings/FindOrRegist.swift
@@ -0,0 +1,21 @@
+//
+//  FindOrRegistration.swift
+//  Whereismythings
+//
+//  Created by JeonghoYang on 2021/01/27.
+//
+
+import Foundation
+
+struct Find {
+    var findItemname : String
+    var findLocation : String
+    var findPicture : String
+    
+}
+
+struct Regist {
+    var registfitemname : String
+    var registLocation : String
+    var registPicture : String
+}
