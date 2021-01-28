d4a78473408c9ebc4658d7cbc11aac166a0d9dee
송낙현 <>
2021-01-28 21:55:56 +0900

    일단 레지스터등록

---
+++ Whereismythings/Whereismythings/1.swift
@@ -0,0 +1,34 @@
+//
+//  FindOrRegisterViewController.swift
+//  Whereismythings
+//
+//  Created by 강희선 on 2020/12/26.
+//
+
+import UIKit
+
+class FindOrRegisterViewController: UITableViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
