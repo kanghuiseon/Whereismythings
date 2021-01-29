6ea9153c8c52c027816f5c8731b0df347ebde069
송낙현 <>
2021-01-29 15:21:54 +0900

    stuff ditailview 디자인 수정

--- Whereismythings/Whereismythings/FindOrRegisterViewController.swift
+++ Whereismythings/Whereismythings/FindOrRegisterViewController.swift
@@ -34,18 +34,18 @@ class FindOrRegisterViewController: UITableViewController {
     @IBAction func lostAndFoundSeg(_ sender: UISegmentedControl) {
         
         
-    switch lostAndFoundSeg.selectedSegmentIndex {
-      case 0:
-        flag = true
-      case 1:
-        flag = false
-      default:
-          break
+        switch lostAndFoundSeg.selectedSegmentIndex {
+          case 0:
+            flag = true
+          case 1:
+            flag = false
+          default:
+              break
 
     
         
         
-    }
+        }
     
     }
     
@@ -53,15 +53,6 @@ class FindOrRegisterViewController: UITableViewController {
 
     @IBAction func ComButtonTapped(_ sender: UIButton) {
        
-        
-        
-
-        
-        
-        
-        
-        print("탭은되고있다")
-        
         let stuffname = stuffNameTextField.text ?? ""
         let stuffinfo = stuffInfoTextField.text ?? ""
         let stuffloca = stuffLocaTextField.text ?? ""
@@ -78,9 +69,7 @@ class FindOrRegisterViewController: UITableViewController {
             
             db.collection("users").document(uid).collection("number").document().setData(data)
             
-//            print(uid)
-//
-//            db.collection("users").document("18KX8EtXmWmayRYPxOuu").collection("stuff").addDocument(data: ["flag":flag,"stuffname":stuffname,"stuffinfo":stuffinfo,"stuffloca":stuffloca])
+
         } else {
             
             return
@@ -89,56 +78,3 @@ class FindOrRegisterViewController: UITableViewController {
     }
 }
         
-        
-//'
-//FirebaseDataService.instance.userRef.observeSingleEvent(of: .value, with: { (snapshot) in
-//    if let data = snapshot.value as? Dictionary<String, AnyObject>, let uid = FirebaseDataService.instance.currentUserUid {
-//        for (key, data) in data {
-//            if uid != key {
-//                if let userData = data as? Dictionary<String, AnyObject> {
-//                    let toUid = key
-//                    let username = userData["name"] as? String
-//                    let email = userData["email"] as? String
-//                    let user = User(uid: toUid, email: email!, username: username!)
-//                    self.userList.append(user)
-//
-//        '
-//let data: [String: Any] = [:]
-//
-//        // [START set_data]
-//        db.collection("cities").document("new-city-id").setData(data)
-//
-//
-//
-        
-        
-        
-        
-        
-//        db.collection("users").document("18KX8EtXmWmayRYPxOuu").collection("stuff").addDocument(data: ["stuffname":stuffname])
-//        db.collection("users").document("18KX8EtXmWmayRYPxOuu").collection("stuff").addDocument(data: ["stuffperson":"송낙현"])
-//        db.collection("users").document("18KX8EtXmWmayRYPxOuu").collection("stuff").addDocument(data: ["stuffinfo":stuffinfo])
-//        db.collection("users").document("18KX8EtXmWmayRYPxOuu").collection("stuff").addDocument(data: ["stuffloca":stuffloca])
-//
-        
-//
-//        print(stuffname)
-//
-//        MainMapModel.stuffs.append(MainMapModel(id: 16, gotTime: nil, stuffPerson: "익명송낙현", flag: flag, stuffName: stuffname, stuffCharacteristic: stuffinfo, stuffLatitudePosition: "37.5613435", stuffLongitudePosition: "127.0408104", stuffKoreanPosition: stuffloca, stuffImage: nil))
-//
-//
-//        print(MainMapModel.stuffs)
-//
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
